3fed0c8a1fc1fb93f3e7c516fa2ed913aa6e35ff
xubo <ABCabc123>
2022-01-10 11:19:39 +0800

    RocketMQ console 安装文章

---
+++ src/test/java/com/xubo/druid/mq/rocketmq/RocketMQTest.java
@@ -0,0 +1,10 @@
+package com.xubo.druid.mq.rocketmq;
+
+/**
+ * @Author xubo
+ * @Date 2022/1/10 10:45
+ * RocketMQ 安装部署 https://cnblogs.com/lzh1043060917/p/15745201.html
+ *          RocketMQs控制台安装 https://blog.csdn.net/starzhou/article/details/105889294
+ */
+public class RocketMQTest {
+}
