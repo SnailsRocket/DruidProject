acdf6abddddf958612b4b61a53e43adc737de23a
xubo <ABCabc123>
2022-01-25 13:39:27 +0800

    copy 整个List集合,要求两个对象的属性一直才可以

---
+++ src/main/java/com/xubo/druid/util/convertlist/DateConvertTarget.java
@@ -0,0 +1,22 @@
+package com.xubo.druid.util.convertlist;
+
+import lombok.Data;
+
+/**
+ * @Author xubo
+ * @Date 2022/1/25 11:03
+ */
+@Data
+public class DateConvertTarget {
+
+    private String name1;
+
+    private Integer age;
+
+    private String address;
+
+    private String tel;
+
+    private String description;
+
+}
