4bf8e663bfd503e5b747461c4703e7be361d534d
xubo <1837039333@qq.com>
2022-01-31 11:47:29 +0800

    投票西永

--- src/main/java/com/xubo/druid/Application.java
+++ src/main/java/com/xubo/druid/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 增量部署  远程调试
  * 详解Executors.newFixedThreadPool()   https://blog.csdn.net/qq_35029061/article/details/86716334
  * reastTemplate事故  https://blog.csdn.net/weixin_45431247/article/details/103627301     https://www.cnblogs.com/jimw/p/9037542.html  https://www.jianshu.com/p/2d05397688dd
+ * 投票系统设计
  *
  */
 @SpringBootApplication
