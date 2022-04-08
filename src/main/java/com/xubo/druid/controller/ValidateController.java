cabc921a4579fbd4cef1182e2a16ee2968bfb3d2
xubo <ABCabc123>
2022-04-08 14:46:34 +0800

    Hibernate-validation 优雅的参数校验

--- src/main/java/com/xubo/druid/controller/ValidateController.java
+++ src/main/java/com/xubo/druid/controller/ValidateController.java
@@ -10,19 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 /**
  * @Author xubo
  * @Date 2022/4/7 17:13
  * Hibernater 参数校验
+ * 关于 Hibernate-validation 的用法
+ * 参考 ：http://www.javashuo.com/article/p-uavnoqla-kz.html
+ * 校验字符串的长度使用 @Size  校验数字的大小使用 @Max @Min
+ * 常见的group 有AddGroup UpdateGroup 也可自定义 group
+ * 控制器入参需要使用 @Validated 或者 @Valid 修饰
+ * Valid 与 Validate 的区别
+ * Valid 没有分组的功能
+ * Validate(group = {AddGroup.class})  只校验实体类中标记了 AddGroup.class 的属性 没标记就都要校验  Validate 是 Valid 的一种封装
  *
  */
 @RestController
 @RequestMapping("/validate")
+//@Validated
 public class ValidateController {
 
 
     @PostMapping("/add")
-    public JSONObject addValidate(@RequestBody @Validated(value = {AddGroup.class}) ValidateEntity validateEntity) {
+    public JSONObject addValidate(@RequestBody @Validated ValidateEntity validateEntity) {
         System.out.println("validateEntity = " + validateEntity);
         return new JSONObject().fluentPut("validateEntity: ", validateEntity);
     }
--- src/main/java/com/xubo/druid/entity/ValidateEntity.java
+++ src/main/java/com/xubo/druid/entity/ValidateEntity.java
@@ -25,13 +25,18 @@ public class ValidateEntity {
     @Null(groups = AddGroup.class, message = "新增id必须为空")
     private Integer id;
 
-    @Max(groups = {AddGroup.class,UpdateGroup.class}, value = 30, message = "姓名长度不超过30！")
+//    @Max(groups = {AddGroup.class,UpdateGroup.class}, value = 30, message = "姓名长度不超过30！")
 //    @Min(groups = {AddGroup.class,UpdateGroup.class}, value = 2, message = "姓名长度不超过2！")
+//    @Size(groups = {AddGroup.class})
     private String name;
 
     @Size(groups = {AddGroup.class,UpdateGroup.class}, max = 11, min = 4, message = "长度不正确！")
     private String tel;
 
     private String address;
+
+    @Min(value = 2,message = "长度不小于2")
+    @Max(value = 100,message = "最大值不能大于100")
+    private Integer num;
 
 }
