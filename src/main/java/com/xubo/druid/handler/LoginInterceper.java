69764f07d6816f98d91839c2bc27f7a4b91bf926
xubo <ABCabc123>
2022-05-17 17:58:07 +0800

    补充

--- src/main/java/com/xubo/druid/handler/LoginInterceper.java
+++ src/main/java/com/xubo/druid/handler/LoginInterceper.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,12 @@ import javax.servlet.http.HttpSession;
 /**
  * @Author xubo
  * @Date 2022/5/17 17:12
+ * 手动实现拦截器
+ * 抽象类 HandlerInterceptorAdapter 也继承了 HandlerInterceptor 接口
+ * 参考博客：https://blog.csdn.net/weixin_44259720/article/details/104615086
+ * 日志记录  可以记录请求信息的日志，以便进行信息监控、信息统计等
+ * 权限校验  如登陆检测，进入处理器检测是否登陆，如果没有直接返回到登陆页面。
+ * 性能监控  典型的是慢日志。
  */
 public class LoginInterceper implements HandlerInterceptor {
 
