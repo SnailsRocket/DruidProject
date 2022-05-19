42e785fe6da709c9e2fe0d22162b18b3baa2a378
xubo <ABCabc123>
2022-05-19 13:04:52 +0800

    note

--- src/test/java/com/xubo/druid/spring/beanlifecycle/MyInstantiationAwareBeanPostProcessor.java
+++ src/test/java/com/xubo/druid/spring/beanlifecycle/MyInstantiationAwareBeanPostProcessor.java
@@ -7,6 +7,22 @@ import org.springframework.beans.factory.config.InstantiationAwareBeanPostProces
 /**
  * @Author xubo
  * @Date 2022/1/5 16:19
+ * 服务划分
+ *  订单服务
+ *  支付服务
+ *  商品服务
+ *  用户服务
+ *  积分服务 (这个可以集成到用户服务)
+ *  认证服务 Oauth2/ SpringSecuroty
+ *  搜索服务
+ *  物流服务
+ *  首页
+ *  消息服务(发送MQ、短信、邮件) 参考Austin
+ *  秒杀服务
+ *  购物车服务(有必要吗?)
+ *  营销服务(配置活动)
+ *
+ *
  */
 public class MyInstantiationAwareBeanPostProcessor extends InstantiationAwareBeanPostProcessorAdapter {
     public MyInstantiationAwareBeanPostProcessor() {
